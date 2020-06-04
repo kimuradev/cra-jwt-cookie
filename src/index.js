4c2956c1570d054ef3bc9a98bac1ca07052ff446
Leandro Kioshi Kimura <leandrokk@jmalucelliti.com.br>
2020-06-04 10:44:50 -0300

    feat: add appCookie and appLocalStorage
    With both examples

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+// import App from './AppLocalStorage';
+
+import App from './AppCookie';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -8,5 +10,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-
